0ffbf11fcf17aceffb2d328d7d8eedca81edb613
Teddy Raga <ibqraga@gmail.com>
2025-04-26 18:44:47 -0600

    autos v3

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # autos
+
+Esta es una aplicación web que analiza la información de una base de datos de anuncios de automoviles.
+
+Puedes seleccionar entre las diferentes casillas para observar datos relevantes sobre los anuncios publicados.
--- app.py
+++ app.py
@@ -4,16 +4,53 @@ import plotly.express as px
 
 car_data = pd.read_csv("C:\\Users\\MANUEL CONTRERAS\\Documents\\autos\\autos\\vehicles_us.csv") #Leer datos
 
-st.header('Vehiculos') #Encabezado 
+st.header('Análisis de anuncios de automóviles') #Encabezado 
 
-hist_button = st.button('Construir histograma') #Boton para crear histograma
+# crear una casilla de verificación
+histogram_model = st.checkbox('¿Cuales son modelos de los autos mas anunciados')
 
-if hist_button: # al hacer clic en el botón
+if histogram_model: # al seleccionar la casilla de verificación
             # escribir un mensaje
-            st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+            st.write('Construir un histograma para la columna model')
             
             # crear un histograma
-            fig = px.histogram(car_data, x="odometer")
+            fig = px.histogram(car_data, x="model")
         
+            # mostrar un gráfico Plotly interactivo
+            st.plotly_chart(fig, use_container_width=True)
+
+histogram_fuel = st.checkbox('¿Cual es el combustible mas utilizados por los autos anunciados?')
+if histogram_fuel: # al seleccionar la casilla de verificación
+            # escribir un mensaje
+            st.write('Construir un histograma para la columna fuel')
+            
+            # crear un histograma
+            fig = px.histogram(car_data, x="fuel")
+        
+            # mostrar un gráfico Plotly interactivo
+            st.plotly_chart(fig, use_container_width=True)
+
+
+# crear una casilla de verificación
+histogram_type = st.checkbox('¿Cuales son los tipos de autos mas anunciados?')
+if histogram_type: # al hacer clic en el botón
+            # escribir un mensaje
+            st.write('Construir un diagrama de dispersión para la columna price')
+
+            # crear un diagrama de dispersión
+            fig = px.histogram(car_data, x="type") # crear un gráfico de dispersión
+
+            # mostrar un gráfico Plotly interactivo
+            st.plotly_chart(fig, use_container_width=True)
+
+# crear una casilla de verificación
+dispersion_price = st.checkbox('¿Cual es el rango de precios anunciados?')
+if dispersion_price: # al hacer clic en el botón
+            # escribir un mensaje
+            st.write('Construir un diagrama de dispersión para la columna price')
+
+            # crear un diagrama de dispersión
+            fig = px.scatter(car_data, y="price") # crear un gráfico de dispersión
+
             # mostrar un gráfico Plotly interactivo
             st.plotly_chart(fig, use_container_width=True)
